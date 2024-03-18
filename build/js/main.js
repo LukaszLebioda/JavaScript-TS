eb92f4a4deaf2b7da09c89f608e6da496626cbd6
llebioda <llebioda@novomatic-tech.com>
2024-03-18 21:26:05 +0100

    new beginning

--- build/js/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const sum = (a, b) => {
-    return a + b;
-};
-let album = true;
-let postId;
-let re = /\w+/g;
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-const sum = (a: number, b: string) => {
-	return a + b
-}
-
-let album: any = true
-
-let postId: string | number
-
-let re: RegExp = /\w+/g
